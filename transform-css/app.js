4c3df8a212f89ca70090a651655bfe81ee0e08d1
Jenna C <jennac0312@gmail.com>
2023-06-15 09:00:13 -0400

    improved... barely

--- transform-css/app.js
+++ transform-css/app.js
@@ -6,11 +6,32 @@ const startSquirm = () => {
 }
 
 
+const crybaby = document.querySelector('.crybaby')
+const crybabyText = document.querySelector('.crybabyText')
 const drainPuddle = () => {
     const puddle = document.querySelector('.puddleContainer')
 
     puddle.classList.toggle('draining')
+
+    switchCryBaby()
 }
+
+const switchCryBaby = () => {
+    // not working grr
+    // setTimeout(switchCryBaby(), 150000)
+    // wait(1000)
+    if(crybaby.src === `https://media3.giphy.com/media/Xp6cyO2oLTM8TLV2R1/giphy.gif?cid=6c09b952d32284bc066d6387ed7c51a12a43b7deb9f02272&ep=v1_internal_gifs_gifId&rid=giphy.gif&ct=s`){
+        crybaby.src = `https://www.icegif.com/wp-content/uploads/2023/04/icegif-1422.gif`
+        crybabyText.innerHTML = `Happy Ducky`
+        crybabyText.setAttribute('style', 'color: white')
+    } else{
+        crybaby.src = `https://media3.giphy.com/media/Xp6cyO2oLTM8TLV2R1/giphy.gif?cid=6c09b952d32284bc066d6387ed7c51a12a43b7deb9f02272&ep=v1_internal_gifs_gifId&rid=giphy.gif&ct=s`
+        crybabyText.innerHTML = `Crybaby`
+        crybabyText.setAttribute('style', 'color: black')
+    }
+}
+
+
 
 const dontHelp = () => {
     console.log('help clicked')
